ad3483590b56af81f96ba5b35a5e7d49232733ac
faraamcode <abdulrasaqalagbede@gmail.com>
2021-03-30 20:58:11 +0100

    combine reducer

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
 const redux = require("redux")
 const createStore = redux.createStore
+const combinereducer = redux.combineReducers
 
 
-const initialState = {
-    count : 21,
-    name : "Yourghut"
+const orangeState = {
+    orange : 21,
+
 }
-const reducer = (state =initialState, action)=>{
-    if(action.type ==="increase"){
+const bannanState = {
+    bannana : 21,
+
+}
+const orangereducer = (state =orangeState, action)=>{
+    if(action.type ==="buy_orange"){
         return {
-            ...state, count: state.count + 1
+            ...state, orange: state.orange + 1
         }
     }
     return state
 }
-
-const store = createStore(reducer)
+const bannanareducer = (state =bannanState, action)=>{
+    if(action.type ==="buy_bannaba"){
+        return {
+            ...state, bannana: state.bannana + 1
+        }
+    }
+    return state
+}
+const combineredux = combinereducer({first: orangereducer, second :bannanareducer})
+const store = createStore(combineredux)
 console.log(store.getState());
 store.subscribe(()=> console.log("update", store.getState()))
 store.dispatch({
-    type: "increase"
+    type: "buy_bannaba"
 });
 store.dispatch({
-    type: "increase"
+    type: "buy_orange"
 });
 store.dispatch({
-    type: "increase"
+    type: "buy_bannaba"
 });
 
 
